e8e4f1ef20a5b41b40b87bf2e1325df793b97dec
miya <709852750@qq.com>
2025-08-02 19:58:42 +0800

    修复委托注册删除问题

--- proj_l2dw/Assets/Scripts/Utils/UIEventBus.cs
+++ proj_l2dw/Assets/Scripts/Utils/UIEventBus.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 public static class UIEventBus
 {
@@ -11,25 +12,33 @@ public static class UIEventBus
     {
         if (eventListeners.TryGetValue(eventType, out var listeners))
         {
-            listeners += callback;
+            listeners = Delegate.Combine(listeners, callback) as Action;
             eventListeners[eventType] = listeners;
+            //Debug.Log("AddListener" + eventType + " " + (listeners.GetInvocationList()?.Length ?? 0));
         }
         else
         {
             eventListeners[eventType] = callback;
+            //Debug.Log("AddListener" + eventType + " " + (callback.GetInvocationList()?.Length ?? 0));
         }
+
     }
 
     public static void RemoveListener(UIEventType eventType, Action callback)
     {
         if (eventListeners.TryGetValue(eventType, out var listeners))
         {
-            listeners -= callback;
+            listeners = Delegate.Remove(listeners, callback) as Action;
             if (listeners == null)
             {
                 eventListeners.Remove(eventType);
+                //Debug.Log("clean " + eventType);
+                return;
             }
+            eventListeners[eventType] = listeners;
+            //Debug.Log("RemoveListener" + eventType + " " + (listeners.GetInvocationList()?.Length ?? 0));
         }
+
     }
 
     public static void SendEvent(UIEventType eventType)
