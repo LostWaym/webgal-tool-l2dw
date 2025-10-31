3a179d7a6c93376d7d5c9b7090dcb68f96ef36ce
miya <709852750@qq.com>
2025-10-31 23:05:05 +0800

    移除莫名其妙的using

--- proj_l2dw/Assets/Scripts/UI/UIItemWidget.cs
+++ proj_l2dw/Assets/Scripts/UI/UIItemWidget.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditorInternal.VersionControl;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
